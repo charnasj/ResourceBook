f9becdd4599063592251cfdc8354cedb088f76d9
iliya <chakampismo@yahoo.com>
2011-05-02 16:46:37 +0200

    Local changes

--- src/ResourceBook/manage.py
+++ src/ResourceBook/manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
-import imp # WAZAAAAAAAA
+import imp
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
@@ -12,5 +12,3 @@ import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
-# WAZAAAAAAAAAAAAAAA
-#COME ON, please
