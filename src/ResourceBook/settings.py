8dcd4a6aa49ccfa01dc7e7ef9c0085360c54a4d9
Jon Charnas <jonathan.charnas@unine.ch>
2011-05-14 15:15:12 +0200

    Fixing errors in models.py.

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/john/ResourceBook/src/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'ManageResources'
 )
 
 # A sample logging configuration. The only tangible logging
