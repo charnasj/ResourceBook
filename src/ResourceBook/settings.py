924ab40077d62e91b3f9be9f9f5acd68de3ec5c8
Jon Charnas <jonathan.charnas@unine.ch>
2011-05-14 16:15:46 +0200

    Changes to the database and settings file.

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -17,13 +17,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '/home/john/ResourceBook/src/sqlite.db',                      # Or path to database file if using sqlite3.
-=======
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite.db',                      # Or path to database file if using sqlite3.
->>>>>>> github/master
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
