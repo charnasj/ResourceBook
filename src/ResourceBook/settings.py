09f8489a4c01cd8d9ed5f602c3d01c95b0221f85
steph77ch <steph77ch@yahoo.fr>
2011-05-19 22:08:25 +0200

    function add_resource + display in admin site

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/users/simon/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
+        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
