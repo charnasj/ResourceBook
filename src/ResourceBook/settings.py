39537efd4f9001398f611c43822e11fa3406df77
jaleh <jaleh.mazaji@unine.ch>
2011-05-20 10:00:47 +0200

    commite example

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -22,7 +22,8 @@ DATABASES = {
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',  
+                            # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -127,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'ManageResources',
+           ' woodPro.blog',
 
 )
 
