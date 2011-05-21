549e765794a5bbbf3b7b2d852424568779cc11f1
jaleh <jaleh.mazaji@unine.ch>
2011-05-21 15:37:32 +0200

    commit dbchange

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
+        'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
