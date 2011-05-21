0eac2f81a1e095f0a9cf02e10edaa5c095e58cc6
steph77ch <steph77ch@yahoo.fr>
2011-05-21 11:36:24 +0200

    test

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -129,10 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    'ManageResources',
-           ' woodPro.blog',
+    # Uncomment the next line to enable admin documentation
     'django.contrib.admindocs',
     'ManageResources'
 )
