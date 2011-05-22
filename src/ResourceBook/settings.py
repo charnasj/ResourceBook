4dea92f0a62d0d6f82c0f739a8d40152d7aa0aaa
jaleh <jaleh.mazaji@unine.ch>
2011-05-22 11:14:07 +0200

    commitupdating

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -16,18 +16,11 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+{
 
-<<<<<<< HEAD
-DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
-=======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.S
-        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
->>>>>>> origin/master
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
