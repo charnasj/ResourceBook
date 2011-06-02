9e1065e7e45bc887898eead0a485068de230e7df
steph77ch <steph77ch@yahoo.fr>
2011-06-02 12:20:04 +0200

    view profile

---
+++ src/ResourceBook/ManageResources/viewProfile.py
@@ -0,0 +1,19 @@
+from django.views.generic.list_detail import object_list
+from django.views.generic.list_detail import object_detail
+from django.views.generic.create_update import create_object
+from django.views.generic.create_update import update_object
+from django.views.generic.create_update import delete_object
+from django.core.urlresolvers import reverse
+ 
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render_to_response
+from ResourceBook.ManageResources.models import GoodsResource
+from ResourceBook.ManageResources.models import Resource
+
+def View_Profile(request):
+    queryset=GoodsResource.objects.all()
+    return render_to_response('ManageResources/view_profile.html' , {'goods':queryset})
+
+    
+def index(request):
+    return render_to_response('ManageResources/index.html')
--- src/ResourceBook/ManageResources/views.py
+++ src/ResourceBook/ManageResources/views.py
@@ -45,3 +45,6 @@ def Add_resource_gymhall_form(request):
 def View_available_goods(request):
     return render_to_response('ManageResources/view_available_goods.html')
 
+def View_profile(request):
+    return render_to_response('ManageResources/view_profile.html')
+
