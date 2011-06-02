045fa420c938a6e02114c05ea88fd479d23f5063
Jon Charnas <jonathan.charnas@unine.ch>
2011-06-02 16:14:43 +0200

    Fixing user login after change of HTML linkage for relative paths.

--- src/ResourceBook/ManageResources/userLogin.py
+++ src/ResourceBook/ManageResources/userLogin.py
@@ -12,6 +12,8 @@ def login_view(request):
             return HttpResponse("You provided a correct username and password!")
         else:
             return HttpResponse("Your account has been disabled!")
+    else:
+        return HttpResponse("Wrong username or password!")
 
 def logout_view(request):
     logout(request)
