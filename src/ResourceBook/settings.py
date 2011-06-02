22ffa8a739f013085170e72e9fd408ce70c00008
jaleh <jaleh.mazaji@unine.ch>
2011-06-02 15:34:18 +0200

    commit newexample

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -16,25 +16,15 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-<<<<<<< HEAD
-                  
-=======
 
->>>>>>> origin/master
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.S
-<<<<<<< HEAD
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-=======
-        'NAME': 'sqlite3.db',                   # Or path to database file if using sqli
-        'USER': '',                             # Not used with sqlite3.
-        'PASSWORD': '',                         # Not used with sqlite3.
-        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
->>>>>>> origin/master
+      
         'PORT': '',  
     }
 } 
@@ -142,7 +132,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation
     'django.contrib.admindocs',
-    'ManageResources'
+    'ManageResources',
+    'paypal.standard.ipn'
+    
 )
 
 # A sample logging configuration. The only tangible logging
