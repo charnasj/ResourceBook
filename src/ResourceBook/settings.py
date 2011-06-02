91347e84da0e1e2f180dda3e0023ad9ad2099f02
jaleh <jaleh.mazaji@unine.ch>
2011-06-02 12:18:18 +0200

    commite dfj

--- src/ResourceBook/settings.py
+++ src/ResourceBook/settings.py
@@ -16,40 +16,19 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-{
-
-<<<<<<< HEAD
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
-=======
-<<<<<<< HEAD
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'c:/users/simon/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
-=======
-<<<<<<< HEAD
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite
-=======
+                  
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.S
-        'NAME': 'D:/Dev/workspace/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
+        'NAME': 'c:/users/jaleh/ResourceBook/src/sqlite3.db',                      # Or path to database file if using sqlite3
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  
                             # Set to empty string for default. Not used with sqlite3.
     }
-}            
+} 
+           
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
