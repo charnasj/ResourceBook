800918e699c0baf560e6be9ae870a2c69a8a353b
steph77ch <steph77ch@yahoo.fr>
2011-06-02 18:03:12 +0200

    update

--- src/ResourceBook/ManageResources/viewProfile.py
+++ src/ResourceBook/ManageResources/viewProfile.py
@@ -10,13 +10,25 @@ from django.shortcuts import render_to_response
 from ResourceBook.ManageResources.models import Customer
 from ResourceBook.ManageResources.models import Order
 from ResourceBook.ManageResources.models import ResourceBookUser
+from django.contrib.auth.models import User
 
 def View_Profile(request):
+    print("<==============================================================>")
     if 'id_profile' in request.GET and request.GET['id_profile']:
-        id = request.GET['id_profile']
-    queryset = Customer.objects.get(pk=id)
-    queryset_order = Order.objects.get(customer_id = queryset.id)
-    queryset_user = User.objects.get(pk = queryset.user)
+        get_id = request.GET['id_profile']
+    print("ID : " + get_id)
+    
+    queryset = Customer.objects.get( User.objects.get(pk = get_id))
+    
+    print(queryset.shipping_address)
+    
+    
+    queryset_user = User.objects.get( queryset.objects.get(user_ptr_id = queryset.id) )
+    
+    
+    
+  #  queryset_order = Order.objects.get(customer_id = queryset.id)
+
     return render_to_response('ManageResources/view_profile.html' , {'customer':queryset, 'order':queryset_order, 'user':queryset_user})
 
     
