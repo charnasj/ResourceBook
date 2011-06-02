4f85ce571b87e3cfe355d33848a2e520388f5aeb
stephane <stefan.donnet@unine.ch>
2011-06-02 13:37:34 +0200

    lalalalalalalalalalalalalala

---
+++ src/ResourceBook/ManageResources/PlaceGoodsOrder.py
@@ -0,0 +1,16 @@
+from django.views.generic.list_detail import object_list
+from django.views.generic.list_detail import object_detail
+from django.views.generic.create_update import create_object
+from django.views.generic.create_update import update_object
+from django.views.generic.create_update import delete_object
+from django.core.urlresolvers import reverse
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render_to_response
+
+from models import *
+
+def placeGoodsOrder(request):
+    return HttpResponse("A new Order has been saved!")
+
+def getGoodsOrderForm(request):
+    return render_to_response('ManageResources/place_goods_order_form.html')
--- src/ResourceBook/ManageResources/urls.py
+++ src/ResourceBook/ManageResources/urls.py
@@ -4,9 +4,12 @@ import views
 import userRegistration
 import addResourcesGoods
 import addgymhall
+import PlaceGoodsOrder
+
+from django.conf import settings
 
 urlpatterns = patterns('', 
-    
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'^list/$', views.LocalGovernment_list, name='LocalGovernment_list'),
     url(r'^add_resources_goods/add_resources_goods/$', addResourcesGoods.Add_resources_goods_save, name ='Add_resources_goods_save'),
     url(r'^add_resources_goods/$', addResourcesGoods.Add_resources_goods_form, name ='Add_resources_goods_form'),
@@ -15,4 +18,6 @@ urlpatterns = patterns('',
     url(r'^detail/(?P<id>\d+)/$', views.LocalGovernment_detail, name='LocalGovernment_detail'),
 	url(r'^add_gymhall/save_resource_gymhall/$', addgymhall.Add_resource_gymall_save, name='Add_resource_gymall_save'),
 	url(r'^add_gymhall/$', addgymhall.Add_resource_gymall_form, name='Add_resource_gymall_form'),
+    url(r'^place_goods_order/$', PlaceGoodsOrder.getGoodsOrderForm, name='Place_goods_order'),
+    url(r'^place_goods_order/create_order$', PlaceGoodsOrder.placeGoodsOrder, name='Place_goods_order_form'),
 )
