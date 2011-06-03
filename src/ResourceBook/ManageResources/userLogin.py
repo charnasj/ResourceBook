d952ab65e51e1e12dee7073fbd368e8d2f7fd09c
Jon Charnas <jonathan.charnas@unine.ch>
2011-06-03 16:52:25 +0200

    Modified login and base.html for user login and user profile to function properly.

--- src/ResourceBook/ManageResources/userLogin.py
+++ src/ResourceBook/ManageResources/userLogin.py
@@ -13,25 +13,18 @@ def login_view(request):
         if user.is_active:
             login(request, user)
             request.session['user_id'] = user.id
-            print request.session['user_id']
             request.session['first_name'] = user.first_name
             if user.is_staff == 0 and not request.session.__contains__('customer_id'):
                 request.session['customer_id'] = ResourceBookUser.objects.get(user = user.id).id 
-                #print "customer_id = " +  request.session['customer_id'] 
-            print request.session['user_id']
             return render_to_response('ManageResources/login_succeed.html', context_instance=RequestContext(request))
         else:
-            return HttpResponse("Your account has been disabled!")
+            return HttpResponse("Your account has been disabled!",context_instance=RequestContext(request))
     else:
-        return render_to_response('ManageResources/login_failed.html')
+        return render_to_response('ManageResources/login_failed.html',context_instance=RequestContext(request))
 
 def logout_view(request):
     logout(request)
-    response = HttpResponse("Successfully logged out. See you again soon!")
-    del request.session['user_id']
-    if request.session['customer_id'] is not None:
-        del request.session['customer_id']
-    return response
+    return render_to_response('ManageResources/logout.html',context_instance=RequestContext(request))
 
 def login_form(request):
-    return render_to_response('ManageResources/login.html')
+    return render_to_response('ManageResources/login.html',context_instance=RequestContext(request))
